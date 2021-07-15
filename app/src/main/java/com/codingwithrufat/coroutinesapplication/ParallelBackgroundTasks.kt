f74f0b9d115dfc709d42d62d14f72c786c652218
Rufat Nasirov <rufet.nesirov2001@mail.ru>
2021-07-15 23:01:45 +0400

    background paralel tasks

---
+++ app/src/main/java/com/codingwithrufat/coroutinesapplication/ParallelBackgroundTasks.kt
@@ -0,0 +1,11 @@
+package com.codingwithrufat.coroutinesapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ParalelBackgroundTasks : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_paralel_background_tasks)
+    }
+}
